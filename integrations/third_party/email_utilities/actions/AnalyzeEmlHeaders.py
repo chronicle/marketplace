753deea1238948bf41f7598973a8c0adcf50f69c
narunk <narunk@google.com>
2025-07-04 13:16:18 +0000

    email_utilities/v36.0

--- integrations/third_party/email_utilities/actions/AnalyzeEmlHeaders.py
+++ integrations/third_party/email_utilities/actions/AnalyzeEmlHeaders.py
@@ -29,7 +29,7 @@ from soar_sdk.SiemplifyUtils import output_handler
 INTEGRATION_NAME = "EmailUtilties"
 SCRIPT_NAME = "Analyze EML Headers"
 EXCLUDED_DOMAINS_ADDRESSES = ["127.0.0.1", "localhost"]
-# TODO: For testing.
+
 EMAIL_ADDRESS_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
 RED_COLOR = "FF0000"
