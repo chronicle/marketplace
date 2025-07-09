6f3b28c3301ef9a00d24b49e8721f299b4b94c81
himshikhar <himshikhar@google.com>
2025-07-09 08:20:41 +0000

    Enrichment 1P migration

--- integrations/third_party/enrichment/actions/EnrichSourceAndDestinations.py
+++ integrations/third_party/enrichment/actions/EnrichSourceAndDestinations.py
@@ -17,8 +17,9 @@ from __future__ import annotations
 from soar_sdk.ScriptResult import EXECUTION_STATE_COMPLETED
 from soar_sdk.SiemplifyAction import SiemplifyAction
 from soar_sdk.SiemplifyUtils import output_handler
+from TIPCommon.rest.soar_api import get_investigator_data
+from TIPCommon.types import SingleJson
 
-INVESTIGATOR_URL = "{}/external/v1/investigator/GetInvestigatorData/{}?format=camel"
 
 ACTION_NAME = "Enrich Source and Destinations"
 
@@ -54,58 +55,85 @@ def get_current_alert(alerts, current_alert):
             return alert
 
 
-def get_sources_and_dest(alert):
+def get_sources_and_dest(alert: SingleJson) -> tuple[list[str], list[str]]:
+    """Get sources and destinations from alert.
+
+    Args:
+        alert (SingleJson): Chronicle alert.
+
+    Returns:
+        tuple[list[str], list[str]]: Tuple containing list of sources and destinations.
+    """
     sources = []
     destinations = []
-    for event_card in alert["securityEventCards"]:
-        sources.extend(event_card["sources"])
-        destinations.extend(event_card["destinations"])
 
-    if sources and isinstance(sources[0], dict):
-        sources = [x["identifier"] for x in sources]
-    if destinations and isinstance(destinations[0], dict):
-        destinations = [x["identifier"] for x in destinations]
-    return (sources, destinations)
+    if "securityEventCards" in alert:
+        for event_card in alert["securityEventCards"]:
+            sources.extend(event_card.get("sources", []))
+            destinations.extend(event_card.get("destinations", []))
+
+        if sources and isinstance(sources[0], dict):
+            sources = [x.get("identifier") for x in sources]
+        if destinations and isinstance(destinations[0], dict):
+            destinations = [x.get("identifier") for x in destinations]
+
+    else:
+        for event_card in alert:
+            for group in event_card.get("fields", []):
+                group_name = group.get("groupName", "").lower()
+                for item in group.get("items", []):
+                    value = item.get("value")
+                    if not value:
+                        continue
+                    if group_name == "source":
+                        sources.append(value)
+                    elif group_name == "destination":
+                        destinations.append(value)
+
+    return sources, destinations
 
 
 @output_handler
 def main():
     siemplify = SiemplifyAction()
     siemplify.script_name = ACTION_NAME
-
-    investigator_res = siemplify.session.get(
-        INVESTIGATOR_URL.format(siemplify.API_ROOT, siemplify.case_id),
+    investigator_res = get_investigator_data(
+        chronicle_soar=siemplify,
+        case_id=siemplify.case_id,
+        alert_identifier=siemplify.current_alert.identifier,
     )
-    investigator_res.raise_for_status()
-    alert = get_current_alert(
-        investigator_res.json()["alerts"],
-        siemplify.current_alert.identifier,
-    )
+    alert = None
+    if "alerts" in investigator_res:
+        alert = get_current_alert(
+            investigator_res["alerts"],
+            siemplify.current_alert.identifier,
+        )
+    else:
+        alert = investigator_res
     (sources, dests) = get_sources_and_dest(alert)
     updated_entities = []
 
     for source in sources:
         for entity in get_alert_entities(siemplify):
-            if entity.identifier == source:
+            if entity.identifier.casefold() == source.casefold():
                 entity.additional_properties.update({"isSource": "true"})
                 updated_entities.append(entity)
                 break
     for dest in dests:
         for entity in get_alert_entities(siemplify):
-            if entity.identifier == dest:
+            if entity.identifier.casefold() == dest.casefold():
                 entity.additional_properties.update({"isDest": "true"})
                 updated_entities.append(entity)
                 break
 
     siemplify.update_entities(updated_entities)
-    status = EXECUTION_STATE_COMPLETED  # used to flag back to siemplify system, the action final status
-    output_message = "output message : Enrichment added."  # human readable message, showed in UI as the action result
-    result_value = (
-        None  # Set a simple result value, used for playbook if\else and placeholders.
-    )
+    status = EXECUTION_STATE_COMPLETED
+    output_message = "output message : Enrichment added."
+    result_value = None
 
     siemplify.LOGGER.info(
-        f"\n  status: {status}\n  result_value: {result_value}\n  output_message: {output_message}",
+        f"\n  status: {status}\n  result_value: {result_value}\n"
+        f"output_message: {output_message}",
     )
     siemplify.end(output_message, result_value, status)
 
