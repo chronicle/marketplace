11a4da438928c19a5bcea732275a0d90c9ccc9d2
iddoa <iddoa@google.com>
2025-07-13 15:22:29 +0300

    revert tests

--- integrations/third_party/git_sync/tests/test_defaults/test_imports.py
+++ integrations/third_party/git_sync/tests/test_defaults/test_imports.py
@@ -14,12 +14,11 @@
 
 from __future__ import annotations
 
-import pathlib
-
 from integration_testing.default_tests.import_test import import_all_integration_modules
+
+from .. import common
 
 
 def test_imports() -> None:
     """Import all integration modules to validate there are no import errors."""
-    integration_path = pathlib.Path(__file__).parent.parent.parent
-    import_all_integration_modules(integration_path)
+    import_all_integration_modules(common.INTEGRATION_PATH)
