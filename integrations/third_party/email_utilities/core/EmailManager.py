fb88a4bfc01ed2135e74ddeda620e1a4ebe7091f
arabindaksha <arabindaksha@google.com>
2025-07-24 14:46:10 +0000

    Admiral Group - Parse Case Wall Email IP addresses separated by / extracted as URL

--- integrations/third_party/email_utilities/core/EmailManager.py
+++ integrations/third_party/email_utilities/core/EmailManager.py
@@ -596,9 +596,12 @@ class EmailUtils:
             matched_urls = EmailUtils.find_urls_regex(body)
 
         for found_url in matched_urls:
+            item = found_url.split("/")
             if "." not in found_url:
                 # if we found a URL like e.g. http://afafasasfasfas; that makes no
                 # sense, thus skip it
+                continue
+            if IPV4_REGEX.match(item[0]) or IPV6_REGEX.match(item[0]):
                 continue
             try:
                 ipaddress.ip_address(found_url)
