dcfc75ba1e76a40982672fc0d624f0dd1b380f12
Jaymin Prajapati <jaymin.prajapati@crestdata.ai>
2025-07-30 14:22:42 +0530

    Resolve comments

--- integrations/third_party/infoblox_threat_defense_with_ddi/core/InfobloxExceptions.py
+++ integrations/third_party/infoblox_threat_defense_with_ddi/core/InfobloxExceptions.py
@@ -6,15 +6,11 @@ class InfobloxException(Exception):
     Common Infoblox Exception
     """
 
-    pass
-
 
 class ItemNotFoundException(InfobloxException):
     """
     Exception for not found (404) errors, e.g., Custom List does not exist
     """
-
-    pass
 
 
 class RateLimitException(InfobloxException):
@@ -22,20 +18,14 @@ class RateLimitException(InfobloxException):
     Exception for rate limit
     """
 
-    pass
-
 
 class InternalSeverError(InfobloxException):
     """
     Internal Server Error
     """
 
-    pass
-
 
 class InvalidIntegerException(InfobloxException):
     """
     Custom exception for invalid integer parameters.
     """
-
-    pass
