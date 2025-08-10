5d0cdfe18d15856b393812b23dbc1963f2592e3a
Amit Joseph <amitjoseph@google.com>
2025-08-10 15:37:58 +0300

    Dev-Env: Added option to uploud to staging env

--- packages/mp/src/mp/dev_env/api.py
+++ packages/mp/src/mp/dev_env/api.py
@@ -78,7 +78,9 @@ class BackendAPI:
             self.token = resp.json()["token"]
             self.session.headers.update({"Authorization": f"Bearer {self.token}"})
 
-    def get_integration_details(self, zip_path: pathlib.Path) -> dict[str, Any]:
+    def get_integration_details(
+        self, zip_path: pathlib.Path, is_staging: bool = False
+    ) -> dict[str, Any]:
         """Get integration details from a zipped package.
 
         Args:
@@ -89,13 +91,17 @@ class BackendAPI:
 
         """
         details_url = f"{self.api_root}/api/external/v1/ide/GetPackageDetails?format=camel"
+        if is_staging:
+            details_url += "&isStaging=true"
         data = base64.b64encode(zip_path.read_bytes()).decode()
         details_payload = {"data": data}
         resp = self.session.post(details_url, json=details_payload)
         resp.raise_for_status()
         return resp.json()
 
-    def upload_integration(self, zip_path: pathlib.Path, integration_id: str) -> dict[str, Any]:
+    def upload_integration(
+        self, zip_path: pathlib.Path, integration_id: str, is_staging: bool = False
+    ) -> dict[str, Any]:
         """Upload a zipped integration package to the backend.
 
         Args:
@@ -107,6 +113,8 @@ class BackendAPI:
 
         """
         upload_url = f"{self.api_root}/api/external/v1/ide/ImportPackage?format=camel"
+        if is_staging:
+            is_staging: bool = False
         data = base64.b64encode(zip_path.read_bytes()).decode()
         upload_payload = {
             "data": data,
