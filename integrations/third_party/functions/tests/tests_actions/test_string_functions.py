6c32b2e2f3afa973b5504915b96ff49cf7763f2c
Sofia <getmansofia00@gmail.com>
2025-08-11 11:48:26 +0300

    Adding String Functions test

--- integrations/third_party/functions/tests/tests_actions/test_string_functions.py
+++ integrations/third_party/functions/tests/tests_actions/test_string_functions.py
@@ -54,8 +54,8 @@ class TestStringToLower:
 
         assert action_output.results.result_value == ALL_LOWER_STR
         assert (
-            action_output.results.output_message
-            == f"{ALL_CAPS_STR} successfully converted to {ALL_LOWER_STR} with lower function"
+                action_output.results.output_message
+                == f"{ALL_CAPS_STR} successfully converted to {ALL_LOWER_STR} with lower function"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -66,8 +66,8 @@ class TestStringToLower:
 
         assert action_output.results.result_value == ALL_LOWER_STR
         assert (
-            action_output.results.output_message
-            == f"{ALL_LOWER_STR} successfully converted to {ALL_LOWER_STR} with lower function"
+                action_output.results.output_message
+                == f"{ALL_LOWER_STR} successfully converted to {ALL_LOWER_STR} with lower function"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -82,8 +82,8 @@ class TestStripString:
 
         assert action_output.results.result_value == STRING_WITH_SPACES.strip()
         assert (
-            action_output.results.output_message
-            == f"{STRING_WITH_SPACES} successfully stripped with strip function"
+                action_output.results.output_message
+                == f"{STRING_WITH_SPACES} successfully stripped with strip function"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -94,8 +94,8 @@ class TestStripString:
 
         assert action_output.results.result_value == ONLY_SPACES.strip()
         assert (
-            action_output.results.output_message
-            == f"{ONLY_SPACES} successfully converted to {ONLY_SPACES.strip()} with strip function"
+                action_output.results.output_message
+                == f"{ONLY_SPACES} successfully converted to {ONLY_SPACES.strip()} with strip function"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -103,7 +103,7 @@ class TestStripString:
 
 class TestStringToUpper:
     UPPER_FN: str = "Upper"
-    
+
     @set_metadata(parameters={"Input": ALL_UPPER_STR,"Function": UPPER_FN})
     def test_string_upper_on_upper_string(self, action_output: MockActionOutput) -> None:
         StringFunctions.main()
@@ -158,13 +158,13 @@ class TestReplaceString:
         "Function": REPLACE_FN,
         "Param 1": "c",
         "Param 2": "d"
-         })
+    })
     def test_string_replace_on_lower_string(self, action_output: MockActionOutput) -> None:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT
         assert (
                 action_output.results.output_message
-                 == f"{REPLACE_STR} successfully converted to {EXPECTED_RESULT} with replace function"
+                == f"{REPLACE_STR} successfully converted to {EXPECTED_RESULT} with replace function"
 
         )
         assert action_output.results.json_output is None
@@ -183,8 +183,8 @@ class TestCountString:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_COUNT
         assert (
-            action_output.results.output_message
-            == f"'a' was found {EXPECTED_RESULT_COUNT} times in '{COUNT_STR}'"
+                action_output.results.output_message
+                == f"'a' was found {EXPECTED_RESULT_COUNT} times in '{COUNT_STR}'"
 
         )
         assert action_output.results.json_output is None
@@ -203,8 +203,8 @@ class TestFindString:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_FIND
         assert (
-            action_output.results.output_message
-            == f"'i' was found at index {EXPECTED_RESULT_FIND} in '{FIND_STR}'"
+                action_output.results.output_message
+                == f"'i' was found at index {EXPECTED_RESULT_FIND} in '{FIND_STR}'"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -221,8 +221,8 @@ class TestRemoveNewLines:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_REMOVE_NEW_LINES
         assert (
-            action_output.results.output_message
-            == f"{REMOVE_NEW_LINES_STR} successfully removed new lines: {EXPECTED_RESULT_REMOVE_NEW_LINES}"
+                action_output.results.output_message
+                == f"{REMOVE_NEW_LINES_STR} successfully removed new lines: {EXPECTED_RESULT_REMOVE_NEW_LINES}"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -230,17 +230,17 @@ class TestRemoveNewLines:
 class TestIsAlpha:
     IS_ALPHA_FN: str = "IsAlpha"
     @set_metadata(parameters={
-    "Input": IS_ALPHA_STR,
-    "Function": IS_ALPHA_FN
+        "Input": IS_ALPHA_STR,
+        "Function": IS_ALPHA_FN
     })
     def test_string_is_alpha_on_lower_string(self, action_output: MockActionOutput) -> None:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_IS_ALPHA
         assert (
-               action_output.results.output_message
-           if f"All characters in {IS_ALPHA_STR} are alphanumeric"
+            action_output.results.output_message
+            if f"All characters in {IS_ALPHA_STR} are alphanumeric"
 
-           else  f"Not all characters in {IS_ALPHA_STR} are alphanumeric"
+            else  f"Not all characters in {IS_ALPHA_STR} are alphanumeric"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -273,15 +273,15 @@ class TestRegexReplace:
     @set_metadata(parameters={
         "Input": REGEX_REPLACE_STR,
         "Function": REGEX_REPLACE_FN,
-       "Param 1":  "\d",
-       "Param 2": "*"
+        "Param 1":  "\d",
+        "Param 2": "*"
     })
     def test_string_regex_replace_on_lower_string(self, action_output: MockActionOutput) -> None:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_REGEX
         assert (
-            action_output.results.output_message
-            == f"{REGEX_STR} successfully converted to {EXPECTED_RESULT_REGEX} with regex replace function"
+                action_output.results.output_message
+                == f"{REGEX_STR} successfully converted to {EXPECTED_RESULT_REGEX} with regex replace function"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -291,15 +291,15 @@ class TestRegexReplace:
 class TestJSONSerialize:
     JSON_SERIALIZE_FN: str = "JSON Serialize"
     @set_metadata(parameters={
-      "Input": JSON_SERIALIZE_STR,
-      "Function": JSON_SERIALIZE_FN
+        "Input": JSON_SERIALIZE_STR,
+        "Function": JSON_SERIALIZE_FN
     })
     def test_string_json_serialize_on_lower_string(self, action_output: MockActionOutput) -> None:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_JSON
         assert (
-            action_output.results.output_message
-            ==  f"{JSON_SERIALIZE_STR} successfully serialized to JSON format"
+                action_output.results.output_message
+                ==  f"{JSON_SERIALIZE_STR} successfully serialized to JSON format"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -317,8 +317,8 @@ class TestRegex:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_REGEX
         assert (
-            action_output.results.output_message
-            == f"Found following values:\n{EXPECTED_RESULT_REGEX}"
+                action_output.results.output_message
+                == f"Found following values:\n{EXPECTED_RESULT_REGEX}"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -337,8 +337,8 @@ class TestDecodeBase64:
         StringFunctions.main()
         assert action_output.results.result_value == EXPECTED_RESULT_DECODE_BASE64
         assert (
-            action_output.results.output_message
-            ==  f"Decoded base64 string to: {EXPECTED_RESULT_DECODE_BASE64}"
+                action_output.results.output_message
+                ==  f"Decoded base64 string to: {EXPECTED_RESULT_DECODE_BASE64}"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state is ExecutionState.COMPLETED
@@ -358,8 +358,8 @@ class TestEncodeBase64:
 
         assert action_output.results.result_value == self.EXPECTED_RESULT_ENCODE_BASE64
         assert (
-            action_output.results.output_message
-            == f"Successfully base64 encoded {self.ENCODE_BASE_64_STR}."
+                action_output.results.output_message
+                == f"Successfully base64 encoded {self.ENCODE_BASE_64_STR}."
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state == ExecutionState.COMPLETED
@@ -380,8 +380,8 @@ class TestLogicOperators:
         # Assert
         assert action_output.results.result_value == EXPECTED_RESULT_LOGIC_OPERATORS
         assert (
-            action_output.results.output_message
-            == f"{LOGIC_OPERATORS_STR} successfully converted to: {EXPECTED_RESULT_LOGIC_OPERATORS}"
+                action_output.results.output_message
+                == f"{LOGIC_OPERATORS_STR} successfully converted to: {EXPECTED_RESULT_LOGIC_OPERATORS}"
         )
         assert action_output.results.json_output is None
         assert action_output.results.execution_state == ExecutionState.COMPLETED
@@ -401,7 +401,7 @@ class TestSplit:
         # Assert
         assert action_output.results.result_value == EXPECTED_RESULT_SPLIT
         assert (
-            action_output.results.output_message
-            == f'Successfully split string {SPLIT_STR} with delimiter "|"'
+                action_output.results.output_message
+                == f'Successfully split string {SPLIT_STR} with delimiter "|"'
         )
         assert action_output.results.execution_state == ExecutionState.COMPLETED
